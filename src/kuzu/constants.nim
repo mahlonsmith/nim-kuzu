0d481fba28a54f02b1733f6e9e0335d44fd102e8
mahlon <mahlon@martini.nu>
2025-04-02 02:22:29 +0000

    Bump version.
    FossilOrigin-Name: 9d5624ddbf6e20ce882fd89016f096b69f40b4cb13ceb142ec8bf8eb49fc69d0

--- src/kuzu/constants.nim
+++ src/kuzu/constants.nim
@@ -1,6 +1,6 @@
 # vim: set et sta sw=4 ts=4 :
 
-const KUZU_VERSION*             = "0.1.0"
+const KUZU_VERSION*             = "0.2.0"
 const KUZU_EXPECTED_LIBVERSION* = "0.9.0"
 
 let KUZU_DEFAULT_CONFIG*  = kuzu_default_system_config()
